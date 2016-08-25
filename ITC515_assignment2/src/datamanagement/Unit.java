88fb332f1177a348fa9d336a046bb4483061d5f6
Andrew Perera <andrew5core@hotmail.com>
2016-08-25 10:43:53 +1000

    Variable names chaned to meaningful ones Unit.Java

--- ITC515_assignment2/src/datamanagement/Unit.java
+++ ITC515_assignment2/src/datamanagement/Unit.java
@@ -1,91 +1,91 @@
 package datamanagement;
 
 public class Unit implements IUnit {
-	private String uc;
-	private String UN;
-	private float co2;
-	private float co1;
-	private float co4;
-	private float co3;
-	private float co5;
-	private int a1, a2, ex;
+	private String unitCode_;
+	private String unitName_;
+	private float column1_;
+	private float column2_;
+	private float column3_;
+	private float column4_;
+	private float column5_;
+	private int assignment1_, assignment2_, exam_;
 	
-	private StudentUnitRecordList rs;
+	private StudentUnitRecordList studentUnitRecordList;
 
-	public Unit(String UC, String un, float f1, float f2, float f3, float f4,
-			float f5, int i1, int i2, int i3, StudentUnitRecordList rl) {
+	public Unit(String unitCode, String unitName, float column1, float column2, float column3, float column4,
+			float column5, int assignment1, int assignment2, int exam, StudentUnitRecordList studentUnitRecordList) {
 
-		uc = UC;
-		UN = un;
-		co2 = f1;
-		co1 = f2;
-		this.co4 = f3;
-		co3 = f4;
-		this.co5 = f5;
-		this.setAssessmentWeights(i1, i2, i3);
-		rs = rl == null ? new StudentUnitRecordList() : rl;
+		this.unitCode_ = unitCode;
+		this.unitName_ = unitName;
+		this.column1_ = column1;
+		this.column2_ = column2;
+		this.column4_ = column3;
+		this.column3_ = column4;
+		this.column5_ = column5;
+		this.setAssessmentWeights(assignment1, assignment2, exam);
+		this.studentUnitRecordList = studentUnitRecordList == null ? new StudentUnitRecordList() : studentUnitRecordList;
 	}
 
 	public String getUnitCode() {
-		return this.uc;
+		return this.unitCode_;
 	}
 
 	public String getUnitName() {
 
-		return this.UN;
+		return this.unitName_;
 	}
 
 	public void setPsCutoff1(float cutoff) {
-		this.co2 = cutoff;
+		this.column1_ = cutoff;
 	}
 
 	public float getPsCutoff() {
-		return this.co2;
+		return this.column1_;
 	}
 
 	public void setCrCutoff(float cutoff) {
-		this.co1 = cutoff;
+		this.column2_ = cutoff;
 	}
 
 	public float getCrCutoff() {
-		return this.co1;
+		return this.column2_;
 	}
 
 	public void setDiCutoff(float cutoff) {
-		this.co4 = cutoff;
+		this.column4_ = cutoff;
 	}
 
 	public float getDiCuttoff() {
-		return this.co4;
+		return this.column4_;
 	}
 
 	public void HDCutoff(float cutoff) {
-		this.co3 = cutoff;
+		this.column3_ = cutoff;
 	}
 
 	public void setHdCutoff(float cutoff) {
-		this.co3 = cutoff;
+		this.column3_ = cutoff;
 	}
 
 	public float getHdCutoff() {
-		return this.co3;
+		return this.column3_;
 
 	}
 
 	public void setAeCutoff(float cutoff) {
-		this.co5 = cutoff;
+		this.column5_ = cutoff;
 	}
 
 	public float getAeCutoff() {
-		return this.co5;
+		return this.column5_;
 	}
 
 	public void addStudentRecord(IStudentUnitRecord record) {
-		rs.add(record);
+		this.studentUnitRecordList.add(record);
 	}
 
 	public IStudentUnitRecord getStudentRecord(int studentID) {
-		for (IStudentUnitRecord r : rs) {
+		for (IStudentUnitRecord r : this.studentUnitRecordList) {
 			if (r.getStudentID() == studentID)
 				return r;
 		}
@@ -93,37 +93,37 @@ public class Unit implements IUnit {
 	}
 
 	public StudentUnitRecordList listStudentRecords() {
-		return rs;
+		return this.studentUnitRecordList;
 	}
 
 	@Override
 	public int getAsg1Weight() {
-		return a1;
+		return this.assignment1_;
 	}
 
 	@Override
 	public int getAsg2Weight() {
-		return a2;
+		return this.assignment2_;
 	}
 
 	@Override
 	public int getExamWeight() {
-		return ex;
+		return this.exam_;
 	}
 
 	@Override
-	public void setAssessmentWeights(int a1, int a2, int ex) {
-		if (a1 < 0 || a1 > 100 ||
-			a2 < 0 || a2 > 100 ||
-			ex < 0 || ex > 100 ) {
+	public void setAssessmentWeights(int assignment1, int assignment2, int exam) {
+		if (assignment1 < 0 || assignment1 > 100 ||
+			assignment2 < 0 || assignment2 > 100 ||
+			exam < 0 || exam > 100 ) {
 			throw new RuntimeException("Assessment weights cant be less than zero or greater than 100");
 		}			
-		if (a1 + a2 + ex != 100) {
+		if (assignment1 + assignment2 + exam != 100) {
 			throw new RuntimeException("Assessment weights must add to 100");
 		}
-		this.a1 = a1;
-		this.a2 = a2;
-		this.ex = ex;			
+		this.assignment1_ = assignment1;
+		this.assignment2_ = assignment2;
+		this.exam_ = exam;			
 	}
 	
 	private void setCutoffs( float ps, float cr, float di, float hd, float ae) {
@@ -152,21 +152,21 @@ public class Unit implements IUnit {
 	public String getGrade(float f1, float f2, float f3) {
 		float t = f1 + f2 + f3;
 		
-		if (f1 < 0 || f1 > a1 ||
-			f2 < 0 || f2 > a2 ||
-			f3 < 0 || f3 > ex ) {
+		if (f1 < 0 || f1 > this.assignment1_ ||
+			f2 < 0 || f2 > this.assignment2_ ||
+			f3 < 0 || f3 > this.exam_ ) {
 			throw new RuntimeException("marks cannot be less than zero or greater than assessment weights");
 		}
 
-		if (t < co5) {
+		if (t < this.column5_) {
 			return "FL";
-		} else if (t < co2)
+		} else if (t < this.column1_)
 			return "AE";
-		else if (t < co1)
+		else if (t < this.column2_)
 			return "PS";
-		else if (t < co4)
+		else if (t < this.column4_)
 			return "CR";
-		else if (t < co3)
+		else if (t < this.column3_)
 			return "DI";
 		else
 			return "HD";
