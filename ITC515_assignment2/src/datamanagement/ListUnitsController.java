269d6ec867bf3d6094e60e18deed0fe1a0def28e
Kishantha Nanayakkara <kishantha@gmail.com>
2016-08-25 22:45:38 +1000

    Renamed ListUnitsCTL to ListUnitsController. Done fixes to ListUnitController

--- ITC515_assignment2/src/datamanagement/ListUnitsController.java
+++ ITC515_assignment2/src/datamanagement/ListUnitsController.java
@@ -1,10 +1,23 @@
 package datamanagement;
-public class ListUnitsCTL {
-    private UnitManager um;
-public ListUnitsCTL() {
-        um = UnitManager.unitMap();
+
+/**
+ * 
+ * ListUnitController will populate a unit list to any iUnitListener
+ *
+ */
+public class ListUnitsController {
+	//This variable will hold a UnitManager instance;
+	private UnitManager unitManager;
+
+	
+	public ListUnitsController() {
+		unitManager = UnitManager.unitMap();
+	}
+
+	public void listUnits(IUnitLister lister) {
+		lister.clearUnits();
+		UnitMap units = unitManager.getUnits();
+		for (String s : units.keySet())
+			lister.addUnit(units.get(s));
+	}
 }
-            public void listUnits( IUnitLister lister ) {
-lister.clearUnits();UnitMap units = um.getUnits();
-        for (String s : units.keySet() )
-            lister.addUnit(units.get(s));}}
